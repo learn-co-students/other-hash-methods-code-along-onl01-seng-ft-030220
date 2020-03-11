089a060578e5350ff1e2ec286e0589d50be45fc6
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-11 16:32:00 +0000

    Done.

--- lib/keys.rb
+++ lib/keys.rb
@@ -8,6 +8,24 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+
+puts groceries.values
+puts "//////////////////////"
+puts groceries.keys
+puts "//////////////////////"
+puts groceries.keys.flatten
+puts "//////////////////////"
+puts groceries.values.flatten.min
+
+
 def get_the_keys(groceries)
-  #code your solution here!
+  groceries.keys
 end
+
+get_the_keys(groceries)
